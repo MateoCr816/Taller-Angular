c0f3ab5657bf5be79d5cb86b5407d51e3d041784
MateoCr816 <111141374+MateoCr816@users.noreply.github.com>
2023-11-08 07:42:27 -0500

    Corrección final

--- src/app/serie/serie.component.ts
+++ src/app/serie/serie.component.ts
@@ -1,20 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { Serie } from './serie';
 import { dataSeries } from './dataSeries';
-import { SerieService } from './serie.service';
 
 @Component({
   selector: 'app-serie',
   templateUrl: './serie.component.html',
   styleUrls: ['./serie.component.css']
-
-})export class SerieComponent implements OnInit {
+})
+export class SerieComponent implements OnInit {
   series: Array<Serie> = [];
+  promedio: number = 0;
+
   constructor() { }
+
+  ngOnInit() {
+    this.series = this.getSerieList();
+    this.calcularPromedio();
+  }
+
   getSerieList(): Array<Serie> {
     return dataSeries;
   }
-  ngOnInit() {
-    this.series = this.getSerieList();
+
+  calcularPromedio() {
+    let totalTemporadas = 0;
+
+    this.series.forEach((serie) => {
+      totalTemporadas += serie.seasons;
+    });
+
+    if (this.series.length > 0) {
+      this.promedio = totalTemporadas / this.series.length;
+    }
   }
 }
